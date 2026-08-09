e21441f5308105c39f05e5121178e8fc39449e64
elsamag <ndusamuelagu@gmail.com>
2026-08-09 21:38:50 +0100

    Delete src/kpi_extraction.sql

--- src/kpi_extraction.sql
+++
@@ -1,9 +0,0 @@
--- Enterprise KPI Extraction Query
--- Author & Lead Technical Consultant: Samuel Chinwendu Agu (Elsamag IT Solutions)
-
-SELECT
-    COUNT(order_id) AS total_orders,
-    SUM(total_amount) AS total_revenue,
-    AVG(total_amount) AS avg_order_value
-FROM
-    orders;
